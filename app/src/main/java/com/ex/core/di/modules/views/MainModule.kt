a4602b3624cead135c0056105a126fbf510e011a
Meshileya Seun <meshileyaseun@gmail.com>
2019-08-18 16:02:38 +0100

    init view modules

---
+++ app/src/main/java/com/ex/core/di/modules/views/MainModule.kt
@@ -0,0 +1,12 @@
+package com.ex.core.di.modules.views
+
+import org.kodein.di.Kodein
+
+/**
+ *@author meshileya seun <mesh@kudi.ai/>
+ *@date 2019-08-18
+ */
+
+val mainModule = Kodein.Module("Main Module") {
+
+}
