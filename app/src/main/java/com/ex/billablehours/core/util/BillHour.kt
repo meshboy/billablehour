50b9f1a82999379c9b1e298ef27ce49bb4227fd3
Meshileya Seun <meshileyaseun@gmail.com>
2019-08-21 13:26:29 +0100

    add helper functions for views and msgs

---
+++ app/src/main/java/com/ex/billablehours/core/util/BillHour.kt
@@ -0,0 +1,27 @@
+package com.ex.billablehours.core.util
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ *@author meshileya seun <mesh@kudi.ai/>
+ *@date 2019-08-21
+ */
+
+
+fun View.snackBar(message: String, length: Int = Snackbar.LENGTH_SHORT) {
+    val snackbar = Snackbar.make(this, message, length)
+    snackbar.show()
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide(isGone: Boolean = true) {
+    visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.INVISIBLE
+    }
+}
